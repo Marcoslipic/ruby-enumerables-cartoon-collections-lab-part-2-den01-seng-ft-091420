f0c0430fedd0d5ab071bc41f7d096edbfb11cde9
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-29 08:00:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,6 +29,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   if (valid_calls[0] == "Earth!")
     return valid_calls[0]
+  end 
   planeteer_calls.find { |str| str == valid_calls.any?}
 end 
 
